9b193d7ddc143a1ccc0283fadcd511d0d3c196d3
duydatpham <duydatpham@gmail.com>
2018-05-08 13:46:15 +0700

    Update Cart.js
    nên dùng state để quản lý cartArray rồi truyền vào CartView hoặc export thẳng CartView luôn.
    
    code hiện tại thì cartArray thay đổi nhưng nó vẫn sẽ không được render lại

--- src/components/Main/Shop/Cart/Cart.js
+++ src/components/Main/Shop/Cart/Cart.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import CartView from './CartView';
 
-class Cart extends Component {
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         cartArray: []
-    //     }
-    //     console.log('cart---------------------');
-    //     console.log(this.props.cartArray);
-    //     console.log('cart---------------------');
-    // }
-    // componentWillMount(nextProps){
-    //     console.log('11111111111111111111111111');
-    //     console.log(nextProps);
-    //     console.log('11111111111111111111111111');
-    //     // this.setState({
-    //     //     cartArray: nextProps.cartArray
-    //     // })
-    // }
-    // componentDidUpdate(){
-    //     console.log('cart---------------------');
-    //     console.log(this.props.cartArray);
-    //     console.log('cart---------------------');
-    // }
-    render() {
-        return (
-            <CartView cartArray={this.props.cartArray}/>
-            // <TouchableOpacity style={{backgroundColor:'red'}} onPress={() => this.props.navigation.navigate('ProductList')}>
-            //     <Text>Cart</Text>
+// class Cart extends Component {
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state={
+//     //         cartArray: []
+//     //     }
+//     //     console.log('cart---------------------');
+//     //     console.log(this.props.cartArray);
+//     //     console.log('cart---------------------');
+//     // }
+//     // componentWillMount(nextProps){
+//     //     console.log('11111111111111111111111111');
+//     //     console.log(nextProps);
+//     //     console.log('11111111111111111111111111');
+//     //     // this.setState({
+//     //     //     cartArray: nextProps.cartArray
+//     //     // })
+//     // }
+//     // componentDidUpdate(){
+//     //     console.log('cart---------------------');
+//     //     console.log(this.props.cartArray);
+//     //     console.log('cart---------------------');
+//     // }
+//     render() {
+//         return (
+//             <CartView cartArray={this.props.cartArray}/>
+//             // <TouchableOpacity style={{backgroundColor:'red'}} onPress={() => this.props.navigation.navigate('ProductList')}>
+//             //     <Text>Cart</Text>
                 
-            // </TouchableOpacity>
-        );
-    }
-}
+//             // </TouchableOpacity>
+//         );
+//     }
+// }
 
-export default Cart;
+export default CartView;
